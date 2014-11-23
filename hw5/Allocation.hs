a62a0a5fe565499e300f6c13c2c5037b38bdc130
Kevin Mannix <kmannix@bu.edu>
2014-11-22 23:55:25 -0500

    Finished alloc traversal

--- hw5/Allocation.hs
+++ hw5/Allocation.hs
@@ -17,6 +17,21 @@ data Graph =
   | Finish Alloc
   deriving (Eq, Show)
 
+leftGraphHelper :: Alloc -> [Item] -> Graph
+leftGraphHelper (Alloc l r) i 
+	| (length i) == 1 = Finish (Alloc (head i + l) r)
+	| otherwise = Branch (Alloc (head i + l) r) (leftGraphHelper (Alloc (head i + l) r) (snd (splitAt 1 i))) (rightGraphHelper (Alloc (head i + l) r) (snd (splitAt 1 i))) 
+
+rightGraphHelper :: Alloc -> [Item] -> Graph
+rightGraphHelper (Alloc l r) i
+	| (length i) == 1 = Finish (Alloc l (head i + r))
+	| otherwise = Branch (Alloc l (head i + r)) (leftGraphHelper (Alloc l (head i + r)) (snd (splitAt 1 i))) (rightGraphHelper (Alloc l (head i + r)) (snd (splitAt 1 i))) 
+
+graph :: Alloc -> [Item] -> Graph
+graph a i 
+	| (length i) == 0 = Finish a
+	| otherwise = Branch a (leftGraphHelper a i) (rightGraphHelper a i)
+
 type Strategy = Graph -> Graph
 
 
