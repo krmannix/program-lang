279aaa754b6c54a83d19ad4a9f2e6dd98af00a23
Kevin Mannix <kmannix@bu.edu>
2014-11-23 01:08:36 -0500

    Finished part 2

--- hw5/Allocation.hs
+++ hw5/Allocation.hs
@@ -12,18 +12,18 @@ type Bin = Integer
 
 data Alloc = Alloc Bin Bin deriving (Eq, Show)
 
-instance Ord Alloc where
-	(Alloc l r) < (Alloc l_ r_) = abs(l-r) < abs(l_-r_)
-	(Alloc l r) <= (Alloc l_ r_) = abs(l-r) <= abs(l_-r_)
-
-data Alpha = Alpha
-instance Eq Alpha where
-    Alpha == Alpha = True
-
 data Graph =
     Branch Alloc Graph Graph 
   | Finish Alloc
   deriving (Eq, Show)
+
+instance Ord Alloc where
+	(Alloc l r) < (Alloc l_ r_) = abs(l-r) < abs(l_-r_)
+	(Alloc l r) <= (Alloc l_ r_) = abs(l-r) <= abs(l_-r_)
+
+instance Ord Graph where
+	g < g' = alloc(g) < alloc(g')
+	g <= g' = alloc(g) <= alloc(g')
 
 leftGraphHelper :: Alloc -> [Item] -> Graph
 leftGraphHelper (Alloc l r) i 
@@ -35,6 +35,19 @@ rightGraphHelper (Alloc l r) i
 	| (length i) == 1 = Finish (Alloc l (head i + r))
 	| otherwise = Branch (Alloc l (head i + r)) (leftGraphHelper (Alloc l (head i + r)) (snd (splitAt 1 i))) (rightGraphHelper (Alloc l (head i + r)) (snd (splitAt 1 i))) 
 
+finalHelper :: Graph -> [Alloc] -> [Alloc]
+finalHelper (Branch a l r) i = (finalHelper l i) ++ (finalHelper r i) ++ i
+finalHelper (Finish a) i = i ++ [a]
+
+depthHelper :: Integer -> Integer -> Graph -> [Alloc] -> [Alloc]
+depthHelper c n (Branch a l r) i 
+	| c == n = i ++ [a]
+	| c > n = []
+	| otherwise = (depthHelper (c+1) n l i) ++ (depthHelper (c+1) n r i) ++ i
+depthHelper c n (Finish a) i 
+	| c == n = i ++ [a]
+	| otherwise = i
+
 graph :: Alloc -> [Item] -> Graph
 graph a i 
 	| (length i) == 0 = Finish a
@@ -43,6 +56,13 @@ graph a i
 alloc :: Graph -> Alloc
 alloc (Branch a _ _) = a
 alloc (Finish a) = a
+
+final ::  Graph -> [Alloc]
+final (Branch a l r) = finalHelper (Branch a l r) []
+final (Finish a) = [a]
+
+depth :: Integer -> Graph -> [Alloc]
+depth n g = depthHelper 0 n g []
 
 type Strategy = Graph -> Graph
 
