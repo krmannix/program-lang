660643e147c9521c8af2d64e03976b47fd1d4688
Kevin Mannix <kmannix@bu.edu>
2014-12-16 15:24:50 -0500

    Finished 3B

--- project/Allocation.hs
+++ project/Allocation.hs
@@ -10,13 +10,17 @@ data Tree a =
   deriving (Eq, Show)
 
 foldTree :: ([a] -> a) -> Tree a -> a
-foldTree _ _ = ??? -- Complete for Problem #3, part (a).
+foldTree f (Finish x) = x
+foldTree f (Branch x (y:ys))  = foldTree f y --foldTree f y
+-- foldTree f c = ??? -- Complete for Problem #3, part (a).
 
 smallest :: Ord a => Tree a -> a
-smallest t = ??? -- Complete for Problem #3, part (b).
+smallest (Finish x) = x
+smallest (Branch x y) = minimum ((x : (map smallest y)))
 
 largest :: Ord a => Tree a -> a
-largest t = ??? -- Complete for Problem #3, part (b).
+largest (Finish x) = x
+largest (Branch x y) = maximum ((x : (map smallest y)))
 
 data Allocation =
     Alloc [(Var, Register)]
@@ -26,8 +30,8 @@ data Allocation =
 -- Add instance declaration for Problem #3, part (c) here.
 
 
-allocations :: (Interference, [Register]) -> Allocation -> [Var] -> Tree Allocation
-allocations (conflicts, rs) (Alloc a) (x:xs) = ??? -- Complete for Problem #3, part (d).
+--allocations :: (Interference, [Register]) -> Allocation -> [Var] -> Tree Allocation
+--allocations (conflicts, rs) (Alloc a) (x:xs) = ??? -- Complete for Problem #3, part (d).
 
 -- Useful helper function.
 unconflicted ::(Interference, [Register]) -> Allocation -> Var -> [Register]
